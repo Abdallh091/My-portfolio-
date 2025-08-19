40281f9567df690c68324244329d72becf62192c
Abdalluh Nasser Ahmed Mansour <abdallhnasser2025@gmail.com>
2025-08-19 03:52:31 +0300

    Update contact Routes.js

--- routes/contact Routes.js
+++ routes/contact Routes.js
@@ -1,1 +1,10 @@
 
+// routes/contactRoutes.js
+const express = require('express');
+const router = express.Router();
+const contactController = require('../controllers/contactController');
+
+// إرسال رسالة اتصال جديدة
+router.post('/', contactController.sendContactMessage);
+
+module.exports = router;
